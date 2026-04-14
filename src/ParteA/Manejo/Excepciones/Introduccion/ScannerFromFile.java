5e2e9a730394412a66e238edc4c22da1b1215238
Hector Montero <h.montero@edu.uah.es>
2026-04-14 19:51:46 +0200

    prueba

--- src/ParteA/Manejo/Excepciones/Introduccion/ScannerFromFile.java
+++ src/ParteA/Manejo/Excepciones/Introduccion/ScannerFromFile.java
@@ -7,7 +7,7 @@ import java.io.File;
 
 public class ScannerFromFile {
     public static void main(String[] args) {
-       //Scanner in = new Scanner(new File("test.in"));
+        //Scanner in = new Scanner(new File("test.in"));
         //System.out.println("Fichero abierto correctamente");
         //in.close();
     }
