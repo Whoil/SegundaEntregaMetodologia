aae2087552e354ccdcb7ec82e45330efaaf4a339
Wally <guillermo.salgado@edu.uah.es>
2026-04-14 17:52:34 +0200

    ñ

---
+++ src/ParteA/Manejo/Excepciones/OperacionesDeManejoDeExcepciones/OperacionesdeManejoDeExcepciones.java
@@ -0,0 +1,23 @@
+package ParteA.Manejo.Excepciones.OperacionesDeManejoDeExcepciones;
+public class OperacionesdeManejoDeExcepciones{
+
+    public static void VerificaciónDeContraseña(String contraseña)  throws Exception{
+        String contraseñaCorrecta="Alvan55";
+        if (!contraseña.equals(contraseñaCorrecta)){
+            throw new Exception("La contraseña es incorrecta");
+        }
+        System.out.println("Contraseña correcta");
+    }
+    public static void ProbarConCatch(){
+        try{
+            VerificaciónDeContraseña("Alban55");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    public static void ProbarConThrow() throws Exception {
+        VerificaciónDeContraseña("alban55");
+    }
+}
+
