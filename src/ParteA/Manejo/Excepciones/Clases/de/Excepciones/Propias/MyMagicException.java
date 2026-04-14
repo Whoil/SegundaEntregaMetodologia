864a32fb9792908fd490df3f80a03bce18f22b80
Wally <guillermo.salgado@edu.uah.es>
2026-04-14 19:41:32 +0200

    ñ

---
+++ src/ParteA/Manejo/Excepciones/Clases/de/Excepciones/Propias/MyMagicException.java
@@ -0,0 +1,8 @@
+package ParteA.Manejo.Excepciones.Clases.de.Excepciones.Propias;
+
+public class MyMagicException extends Exception {
+    public MyMagicException(String message) {  //constructor
+        super(message);
+    }
+
+}
