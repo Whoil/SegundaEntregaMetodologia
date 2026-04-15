d0b2dd42c40adc20a04ace0e11e6513cd7af1222
Wally <guillermo.salgado@edu.uah.es>
2026-04-15 11:08:04 +0200

    ñ

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+import ParteA.Manejo.Excepciones.Introduccion1_1.ScannerFromFileWithCatch;
+import ParteA.Manejo.Excepciones.Introduccion1_1.ScannerFromFileWithThrow;
+
+import ParteA.Manejo.Excepciones.Pila.de.Llamadas1_2.MethodCallStackDemo;
+import ParteA.Manejo.Excepciones.Pila.de.Llamadas1_2.MethodCallStackDemoArithmeticException;
+
+import ParteA.Manejo.Excepciones.OperacionesDeManejoDeExcepciones1_5.OperacionesdeManejoDeExcepciones;
+
+import ParteA.Manejo.Excepciones.try_catch_finally1_6.TryCatchFinallyPrimerEjemplo;
+import ParteA.Manejo.Excepciones.try_catch_finally1_6.MethodCallStackDemoWithFinally;
+
+import ParteA.Manejo.Excepciones.
+
+
+
+
+
+
+
+
 public class Main {
     public static void main(String[] args){
     }
--- src/ParteA/Manejo/Excepciones/Clases/de/Excepciones/Propias1_8/EdadInvalidaException.java
+++ src/ParteA/Manejo/Excepciones/Clases/de/Excepciones/Propias1_8/EdadInvalidaException.java
@@ -1,4 +1,4 @@
-package ParteA.Manejo.Excepciones.Clases.de.Excepciones.Propias1_7;
+package ParteA.Manejo.Excepciones.Clases.de.Excepciones.Propias1_8;
 
 public class EdadInvalidaException extends Exception {
     public EdadInvalidaException(String message) {
